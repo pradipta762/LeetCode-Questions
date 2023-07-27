5a437761ef1a17f721dc710ba0b7fd86193d6978
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2023-07-27 08:17:08 +0530

    Create CountCommonWordsWithOneOccurrence.java
    count common words with one occurrence

---
+++ CountCommonWordsWithOneOccurrence.java
@@ -0,0 +1,45 @@
+/*
+ * exactly once in each of the two arrays.
+
+ 
+
+Example 1:
+
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+Example 2:
+
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+Example 3:
+
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+ */
+public class CountCommonWordsWithOneOccurrence {
+    public int countWords(String[] words1, String[] words2) {
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+        for (String str : words1) {
+            map1.put(str, map1.getOrDefault(str, 0) + 1);
+        }
+        for (String str : words2) {
+            map2.put(str, map2.getOrDefault(str, 0) + 1);
+        }
+        int count = 0;
+        for (String word : words1) {
+            if (map1.get(word) == 1 && map2.getOrDefault(word, 0) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
