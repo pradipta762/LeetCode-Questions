6d6036d33c43eb6b5def4d8b0a7bcb1c0c5392e5
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2023-08-14 07:48:02 +0530

    Create MaximumSubarrayI.java
    maximum subarray i

---
+++ MaximumSubarrayI.java
@@ -0,0 +1,31 @@
+/*
+ * You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+ */
+public class MaximumSubarrayI {
+    public double findMaxAverage(int[] nums, int k) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        double result = sum[k - 1] * 1.0 / k;
+        for (int i = k; i < nums.length; i++) {
+            result = Math.max(result, (sum[i] - sum[i - k]) * 1.0 / k);
+        }
+        return result;
+    }
+}
