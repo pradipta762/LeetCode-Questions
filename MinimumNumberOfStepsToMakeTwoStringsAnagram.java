39ef8173e645fccd561ffa63f0c60cd5c8392895
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2023-08-16 08:23:39 +0530

    Create MinimumNumberOfStepsToMakeTwoStringsAnagram.java
    steps to make two strings anagram

---
+++ MinimumNumberOfStepsToMakeTwoStringsAnagram.java
@@ -0,0 +1,42 @@
+/*
+ * You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+ 
+
+Example 1:
+
+Input: s = "bab", t = "aba"
+Output: 1
+Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+Example 2:
+
+Input: s = "leetcode", t = "practice"
+Output: 5
+Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+Example 3:
+
+Input: s = "anagram", t = "mangaar"
+Output: 0
+Explanation: "anagram" and "mangaar" are anagrams. 
+ */
+public class MinimumNumberOfStepsToMakeTwoStringsAnargram {
+    public int minSteps(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        int count = 0;
+        for (char ch : t.toCharArray()) {
+            if (map.containsKey(ch) && map.get(ch) > 0) {
+                map.put(ch, map.get(ch) - 1);
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
